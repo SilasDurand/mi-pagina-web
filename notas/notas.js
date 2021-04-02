b7244039db8a38c50f89bf069e2e697041e52b80
silas <silasdurandneyra@gmail.com>
2021-04-02 08:32:49 -0500

    mi primer commit

---
+++ notas/notas.js
@@ -0,0 +1,29 @@
+/**Estructurar el contenido
+ * tener un archivo index
+ * Navegacion ---nav
+ * 
+ * header parte de la pgina
+ * 
+ * >>>Fotter pie de pagina
+ *  
+ * 
+ * 
+ * ************************
+ * 
+ * Etiquetas para agrupar
+ * superior <header>
+ * Inferior <footer>
+ * navegacion <nav>
+ * contenido principal <main>
+ * seccion <section>
+ * Entrada de blo o noticias <article>
+ * Barra laterar <aside>
+ * 
+ * si no se llega a usar solo usar el <div>
+ * 
+ * para hacer mas rapido y cargue la hoja de eastulo
+ * 
+ * preload as style
+*///
+
+
